e3927250b10137ce473faf90e533b03f269cc5b6
ldreuillat35 <ldreuillat@ausy.fr>
2020-08-28 12:06:01 +0200

    Lab1 Etape4

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src="./assets/ausyball.png" alt="Logo" />
+            Hire them all
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <HomePage />
+    </main>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+body {
+  background: linear-gradient(to bottom, red, yellow);
+  background-attachment: fixed;
+}
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 500px;
+}
+header {
+  background-color: yellow;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid black;
+}
+.logo {
+  vertical-align: middle;
+  height: 50px;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="geek" src="../assets/geek-home.png" aria-hidden="true" alt="Geek" />
+    </div>
+    <div>
+      <a class="hire-them-all" href="">AUSYMON – Embauchez les tous!</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+.geek {
+  margin-bottom: 20px;
+  height: 300px;
+}
+.hire-them-all {
+  color: black;
+  font-weight: bold;
+  background-color: red;
+  padding: 1em 1.5em;
+  text-decoration: none;
+}
+a:hover {
+  color: white;
+  cursor: pointer;
+}
+</style>
