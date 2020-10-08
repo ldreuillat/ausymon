8d91279e1ba04e7427b6d6ec134217175267ba40
ldreuillat35 <ldreuillat@ausy.fr>
2020-10-08 13:26:56 +0200

    GEstion de la conversion des devises

--- src/shared/salary-filter.js
+++ src/shared/salary-filter.js
@@ -1,7 +1,8 @@
 export default function (salary) {
   const userLang = navigator.language || navigator.userLanguage;
   const currency = userLang.includes('en') ? 'USD' : 'EUR';
-  const money = new Intl.NumberFormat(userLang, { style: 'currency', currency: `${currency}` }).format(salary);
+  const value = userLang.includes('en') ? salary * 1.18 : salary;
+  const money = new Intl.NumberFormat(userLang, { style: 'currency', currency: `${currency}` }).format(value);
 
   return money;
 }
