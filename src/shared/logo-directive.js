47c854d222919d94d769438e324026263df53eac
ldreuillat35 <ldreuillat@ausy.fr>
2020-10-08 11:50:38 +0200

    Lifecycle hook complet sur la directive

--- src/shared/logo-directive.js
+++ src/shared/logo-directive.js
@@ -1,19 +1,17 @@
 /* eslint no-param-reassign: "error" */
-export default {
-  bind: (element, binding) => {
-    const { effect } = binding.value;
-    const { height } = binding.value;
+export default function (element, binding) {
+  const { effect } = binding.value;
+  const { height } = binding.value;
 
-    element.style.verticalAlign = 'middle';
+  element.style.verticalAlign = 'middle';
+  element.style.height = '50px';
+
+  element.addEventListener('mouseenter', () => {
+    element.style.filter = `${effect}`;
+    element.style.height = `${height}`;
+  });
+  element.addEventListener('mouseleave', () => {
+    element.style.filter = 'initial';
     element.style.height = '50px';
-
-    element.addEventListener('mouseenter', () => {
-      element.style.filter = `${effect}`;
-      element.style.height = `${height}`;
-    });
-    element.addEventListener('mouseleave', () => {
-      element.style.filter = 'initial';
-      element.style.height = '50px';
-    });
-  },
-};
+  });
+}
